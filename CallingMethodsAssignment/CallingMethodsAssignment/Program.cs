bcad71a530c4b03cff5322ba1c16fed33fc14183
LoafyLilu <liluloaf@gmail.com>
2023-06-26 11:25:56 -0700

    commit and push assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+                This block will create a new object of the class Time, using
+                the users input. It is converted from data type string to int,
+                and used as the arguments for the appropriate method
+            */
+
+            Time time1 = new Time();
+            Console.WriteLine("Hey! Want to see how old you -really- are?");
+            Console.WriteLine("Please enter your age");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(time1.monthsOld(userAge));
+            Console.WriteLine("Wow, thats a lot of months!");
+
+            Console.WriteLine(time1.weeksOld(userAge));
+            Console.WriteLine("Wow, thats a lot of weeks!");
+
+            Console.WriteLine(time1.daysOld(userAge));
+            Console.WriteLine("Wow, thats a lot of days!");
+
+            Console.ReadLine();
+        }
+        
+
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Time.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    // Creates the class of Time
+    public class Time
+    {
+        //Method to calculate the users age in months
+        public int monthsOld(int userAge)
+        {
+            return userAge * 12;
+        }
+
+        //Method to calculate the users age in weeks
+        public int weeksOld(int userAge)
+        {
+            return userAge * 52;
+        }
+
+        //Method to calculate the users age in days
+        public int daysOld(int userAge)
+        {
+            return userAge * 365;
+        }
+    }
+
