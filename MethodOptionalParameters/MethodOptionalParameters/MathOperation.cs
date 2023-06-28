d3d241ecda93f4f7aeac27cfd99aaa5b6e5c7f9a
LoafyLilu <liluloaf@gmail.com>
2023-06-27 18:25:38 -0700

    commit and push assignment

---
+++ MethodOptionalParameters/MethodOptionalParameters/MathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class MathOperation
+    {
+
+        /*
+        This method sets the value of num1 , and sets the optional value of num2
+        */
+
+        public int AddNumber(int num1, int num2 = 0 )
+        {
+            return num1 + num2;
+        }
+    }
+
---
+++ MethodOptionalParameters/MethodOptionalParameters/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                /*
+                    The following block of code runs if both numbers are entered by the user.
+                    The console parses through the user input to see if the input contains two values.
+                    If so, the block completes.
+                */
+
+                MathOperation operation1 = new MathOperation();
+                Console.WriteLine("Please enter a number:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Pick another number - this is optional");
+                int num2;
+                bool num1Result = Int32.TryParse(Console.ReadLine(), out num2);
+
+                int result = operation1.AddNumber(num1, num2);
+
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+
+            catch
+            {
+                /*
+                    This block will run if the user input for num2 is left blank.
+                    , passing in only the defined value of num1
+                */
+                MathOperation operation2 = new MathOperation();
+                int num1;
+                bool numberResult = Int32.TryParse(Console.ReadLine(), out num1);
+                int result = operation2.AddNumber(num1);
+
+                Console.WriteLine(result);
+
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
