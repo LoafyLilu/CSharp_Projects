7b325dcdf9f8bd142bb8a3ad1f2780ebc7c5b019
LoafyLilu <liluloaf@gmail.com>
2023-06-27 21:10:42 -0700

    commit and push assignment

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Instanciates the class, and passes in the values of
+                5, and 4 into the method AddThings
+            */
+            Stuff thing1 = new Stuff();
+            thing1.AddThings(5, 4);
+
+            /*
+                creates another instance of the class, and passes in
+                the parameter names into the method AddThings
+            */
+            int a = 4;
+            int b = 5;
+            Stuff thing2 = new Stuff();
+            thing1.AddThings(a , b);
+
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Stuff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Stuff
+{
+    /*
+        Creates the void method AddThings, and the variable parameters
+        of int a and int b. The Console then prints the math operation of
+        the first variable, and prints the value of the second.
+    */
+
+    public void AddThings(int a, int b )
+        {
+            Console.WriteLine((a + 2));
+            Console.WriteLine(b);
+        }
+}
