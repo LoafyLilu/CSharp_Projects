268defb7eb66c982058ff89dc2443337511da271
LoafyLilu <liluloaf@gmail.com>
2023-07-19 13:13:11 -0700

    commit and push assignment

---
+++ OverloadingOperators/OverloadingOperators/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    class Employee
+    {
+        //Creates the Class Employee with 3 properties
+        public int Id;
+        public string FirstName;
+        public string LastName;
+
+
+
+        //Overloads the = operator
+        public static bool operator ==(Employee e1, Employee e2)
+        {
+            if (e1.Id.Equals(e2.Id))
+                return true;
+            else
+                return false;
+
+
+            
+        }
+
+        //Overloads the != operator
+        public static bool operator !=(Employee e1, Employee e2)
+        {
+            if (e1.Id.Equals(e2.Id))
+                return true;
+            else
+                return false;
+
+        }
+        
+
+    }
+}
---
+++ OverloadingOperators/OverloadingOperators/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                The following block creates two instances of the Employee class with their assigned properties. It then runs the overloaded operator method on the Id property, comparing the two for equality and prints a unique message to the user
+                based on the bool results
+            */
+
+            Employee e1 = new Employee();
+            e1.Id = 1;
+            e1.FirstName = "Charissa";
+            e1.LastName = "Day";
+
+            Employee e2 = new Employee();
+            e2.Id = 2;
+            e2.FirstName = "Gabe";
+            e2.LastName = "Parker";
+
+            Console.WriteLine(e1.Id.Equals(e2.Id) ? "Ooops! These employees have the same ID!" : "Great! Both employees have unique IDs!");
+
+            Console.ReadLine();
+            
+
+
+            
+
+            
+
+        }
+    }
+}
