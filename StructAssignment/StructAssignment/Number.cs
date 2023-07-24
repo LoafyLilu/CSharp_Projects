4982c72ba2cdf8aed0d5f53fdecd066cee01c5af
LoafyLilu <liluloaf@gmail.com>
2023-07-24 12:37:12 -0700

    commit and push assignment

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    // Creates the struct Number, and gives it a property of Amount
+    // without assigning a value
+    struct Number
+    {
+        public double Amount { get; set; }
+
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciates an object of Number and assigns a value
+            Number number1 = new Number();
+            number1.Amount = 10.5;
+
+            //prints the value of number1 to the console
+            Console.WriteLine(number1.Amount);
+            Console.ReadLine();
+
+            //Instanciates additional struct objects and displays their values
+
+            Number number2 = new Number();
+            Number number3 = number2;
+            number2.Amount = 20.4;
+            number3.Amount = 15.6;
+
+            Console.WriteLine(number2.Amount);
+            Console.ReadLine();
+
+            Console.WriteLine(number3.Amount);
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
